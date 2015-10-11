743a2bfdde9baa375dc0bd480b13c8678870add2
John Anderson <sontek@gmail.com>
2015-10-11 16:38:27 +0800

    f

--- pi_b_plus/demo_2.py
+++ pi_b_plus/demo_2.py
@@ -1,7 +1,7 @@
 # fail
 import cv2
 cameraCapture = cv2.VideoCapture(0)
-fps = 30 # an assumption
+fps = 3 # an assumption
 size = (int(cameraCapture.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)),int(cameraCapture.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)))
 videoWriter = cv2.VideoWriter('MyOutputVid.avi', cv2.cv.CV_FOURCC('I','4','2','0'), fps, size)
 success, frame = cameraCapture.read()
