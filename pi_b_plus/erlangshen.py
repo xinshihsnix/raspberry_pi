eb27295a0791be44978faf4b1e87611a776a2944
xinshi <xinshihsnix@sina.cn>
2015-11-21 23:53:44 +0800

    fix

--- pi_b_plus/erlangshen.py
+++ pi_b_plus/erlangshen.py
@@ -3,7 +3,7 @@ import cv2
 import time
 
 from utils import time_now_str
-
+import requests
 # capture = cv2.VideoCapture(0)
 #
 # print capture.isOpened()
@@ -13,20 +13,46 @@ from utils import time_now_str
 #     cv2.imwrite(time_now_str()+'.jpg', img)
 #     capture.release()
 
-IMG_SAVE_URL = '/var/pi_media/'
+IMG_SAVE_PATH = '/home/pi/what_i_see/'
+PER_IMG_INTERVAL = 5    # 间隔时间, 单位为秒
+
+
+class FileUploader(object):
+    def __init__(self, file_dir='', filename='', dest_url=''):
+        self.file_dir = file_dir
+        self.filename = filename
+        self.dest_url = dest_url
+        self.file_path = self.file_dir + self.filename
+
+    def upload_file(self):
+        files={
+                'pi_img':(self.filename,open(self.file_path,'rb'),'application/octet-stream'),
+               }
+        req = requests.post(url = self.dest_url, files = files)
+        return req
+
+
+    @classmethod
+    def upload_to_xsite(cls):
+        loader = cls(file_dir=IMG_SAVE_PATH, filename='%s.jpg'%time_now_str(), dest_url='http://192.168.1.102:9527/upload_file/')
+        loader.upload_file()
+
 
 class Divineye(object):
     def __init__(self):
         self.capture = cv2.VideoCapture(0)
-        self.img_save_url = IMG_SAVE_URL
+        self.img_save_path = IMG_SAVE_PATH
 
     def save_img(self):
         ret, img = self.capture.read()
-        cv2.imwrite(self.img_save_url + time_now_str()+'.jpg', img)
+        cv2.imwrite(self.img_save_path + time_now_str()+'.jpg', img)
         self.capture.release()
 
 if __name__ == '__main__':
     while True:
         divineye = Divineye()
         divineye.save_img()
-        time.sleep(5)
+        time.sleep(1)
+        FileUploader.upload_to_xsite()
+        time.sleep(PER_IMG_INTERVAL)
+        break
---
+++ pi_b_plus/simple_cv.py
@@ -0,0 +1,3 @@
+# coding: utf-8
+# install SimpleCV  sudo pip install https://github.com/sightmachine/SimpleCV/zipball/develop
+
